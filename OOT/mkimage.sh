237f1b5b21e139e8dbb9be4089902630851a5891
Oleksii Shevchuk <alxchk@gmail.com>
2018-11-20 19:29:34 +0200

    Add default config and build script

---
+++ OOT/mkimage.sh
@@ -0,0 +1,181 @@
+#!/bin/sh
+
+set -e
+
+SELF=`readlink -f "$0"`
+OOT=`dirname "$SELF"`
+KDIR=`readlink -f "$OOT/../"`
+
+KVER=`make -sC "$KDIR" kernelrelease`
+
+OPENSSL_DEBIAN_1_0_VER=${OPENSSL_DEBIAN_1_0_VER:-"1.0.2l-2+deb9u3"}
+OPENSSL_DEBIAN_1_1_VER=${OPENSSL_DEBIAN_1_1_VER:-"1.1.0f-3+deb9u2"}
+
+OPENSSL_DEBIAN_1_0_URL="http://ftp.us.debian.org/debian/pool/main/o/openssl1.0/libssl1.0.2_${OPENSSL_DEBIAN_1_0_VER}_armel.deb"
+OPENSSL_DEBIAN_1_1_URL="http://ftp.us.debian.org/debian/pool/main/o/openssl/libssl1.1_${OPENSSL_DEBIAN_1_1_VER}_armel.deb"
+
+OPENSSL_DEBIAN_1_0_DEV_URL="http://ftp.us.debian.org/debian/pool/main/o/openssl1.0/libssl1.0-dev_${OPENSSL_DEBIAN_1_0_VER}_armel.deb"
+OPENSSL_DEBIAN_1_1_DEV_URL="http://ftp.us.debian.org/debian/pool/main/o/openssl/libssl-dev_${OPENSSL_DEBIAN_1_1_VER}_armel.deb"
+
+DIST=${DIST:-../../bananapi-image}
+DIST=`readlink -f "$DIST"`
+
+CHOST=${CHOST:-armv7a-hardfloat-linux-gnueabi}
+CROSS_COMPILE=${CHOST}-
+CC=${CROSS_COMPILE}gcc
+
+echo "{ CHOST: ${CHOST} }"
+
+KMAKE_VARS="${KMAKE_VARS} INSTALL_MOD_PATH=${DIST} CROSS_COMPILE=${CROSS_COMPILE}"
+KMAKE_VARS="${KMAKE_VARS} INSTALL_HDR_PATH=${DIST}/usr/src/linux"
+
+KMAKE="make ${KMAKE_VARS} -C ${KDIR}"
+
+if [ ! -f ${KDIR}/.config ]; then
+    echo "[+] Copy default config"
+    cp -vf ${OOT}/bananapi_r2_config ${KDIR}/.config
+fi
+
+echo "BUILD: $KVER to $DIST (DIST)"
+rm -rf ${DIST}
+mkdir -p ${DIST}/{boot,lib/firmware}
+
+echo "[+] Copy WIFI firmware & config"
+cp -a ${OOT}/mediatek ${DIST}/lib/firmware/
+
+echo "[+] Copy SATA firmware & flasher"
+mkdir -p ${DIST}/sbin
+cp ${OOT}/ASM1061/ahci420g.rom ${DIST}/lib/firmware/
+cp ${OOT}/ASM1061/106flash ${DIST}/sbin/
+
+echo "[+] Build kernel"
+${KMAKE} -j4
+
+echo "[+] Build u-boot image (zImage + dtb)"
+cat ${KDIR}/arch/arm/boot/{zImage,dts/mt7623n-bananapi-bpi-r2.dtb} \
+    > ${KDIR}/arch/arm/boot/zImage-dtb
+mkimage -A arm -O linux -T kernel -C none -a 80008000 -e 80008000 \
+    -n "Linux" -d ${KDIR}/arch/arm/boot/zImage-dtb ${DIST}/boot/uImage
+
+echo "[+] Install modules"
+${KMAKE} modules_install
+
+echo "[+] Install headers"
+${KMAKE} headers_install
+
+echo "[+] Fix symlinks"
+rm -f ${DIST}/lib/modules/$KVER/{build,source}
+ln -s ../../../usr/src/linux ${DIST}/lib/modules/$KVER/build
+ln -s ../../../usr/src/linux ${DIST}/lib/modules/$KVER/source
+
+echo "{ BUILD OOT MODULES & TOOLS }"
+
+echo "[+] Build cryptodev"
+make -C ${OOT}/cryptodev-linux KERNEL_DIR=${KDIR}
+make -C ${OOT}/cryptodev-linux KERNEL_DIR=${KDIR} DESTDIR=${DIST} prefix=/usr \
+    MAKE="make INSTALL_MOD_PATH=${DIST}" install
+
+echo "[+] Build libmnl (wireguard wg dep)"
+cd ${OOT}/libmnl
+./autogen.sh
+./configure --enable-static --host=${CHOST} --prefix=/usr
+make
+make DESTDIR=${DIST} install
+cd -
+
+echo "[+] Build wireguard module"
+${KMAKE} M=${OOT}/WireGuard/src modules
+${KMAKE} M=${OOT}/WireGuard/src modules_install
+
+echo "[+] Build wireguard cli tool"
+make -C ${OOT}/WireGuard/src/tools \
+    PLATFORM=banana \
+    LDLIBS="${DIST}/usr/lib/libmnl.a" \
+    CC="${CC} -O2 -I${DIST}/usr/include"
+    
+make -C ${OOT}/WireGuard/src/tools \
+    PLATFORM=linux \
+    DESTDIR=${DIST} \
+    WITH_WGQUICK=yes WITH_SYSTEMDUNITS=yes \
+    install
+
+echo "[+] Build exfat kernel module"
+${KMAKE} M=${OOT}/exfat-nofuse CONFIG_EXFAT_FS=m modules
+${KMAKE} M=${OOT}/exfat-nofuse CONFIG_EXFAT_FS=m modules_install
+
+echo "[-] Remove libmnl.a"
+rm -rf ${DIST}/usr/lib
+
+echo "[+] Build af_alg openssl (1.0.2) engine <manually>"
+cd ${OOT}/af_alg/src
+if [ ! -d openssl_dist ]; then
+    mkdir openssl_dist
+
+    echo "[D] Download openssl 1.0.2 libs (${OPENSSL_DEBIAN_1_0_URL})"
+    wget "${OPENSSL_DEBIAN_1_0_URL}" -O openssl_dist/openssl1_0.deb
+    echo "[D] Download openssl 1.0.2 headers (${OPENSSL_DEBIAN_1_0_DEV_URL})"
+    wget "${OPENSSL_DEBIAN_1_0_DEV_URL}" -O openssl_dist/openssl-dev1_0.deb
+
+    dpkg -x openssl_dist/openssl1_0.deb openssl_dist/root1_0
+    dpkg -x openssl_dist/openssl-dev1_0.deb openssl_dist/root1_0
+    
+    echo "[D] Download openssl 1.1.x libs (${OPENSSL_DEBIAN_1_1_URL})"
+    wget "${OPENSSL_DEBIAN_1_1_URL}" -O openssl_dist/openssl1_1.deb
+    echo "[D] Download openssl 1.1.x headers (${OPENSSL_DEBIAN_1_1_DEV_URL})"
+    wget "${OPENSSL_DEBIAN_1_1_DEV_URL}" -O openssl_dist/openssl-dev1_1.deb
+
+    dpkg -x openssl_dist/openssl1_1.deb openssl_dist/root1_1
+    dpkg -x openssl_dist/openssl-dev1_1.deb openssl_dist/root1_1
+fi
+
+OPENSSL_1_0_ROOT=`readlink -f openssl_dist/root1_0`
+OPENSSL_1_0_CFLAGS="-I${OPENSSL_1_0_ROOT}/usr/include -I${OPENSSL_1_0_ROOT}/usr/include/arm-linux-gnueabi"
+OPENSSL_1_0_LIBS="${OPENSSL_1_0_ROOT}/usr/lib/arm-linux-gnueabi/libcrypto.so.1.0.2"
+OPENSSL_1_0_ENGINES=${DIST}/usr/lib/arm-linux-gnueabihf/openssl-1.0.2/engines
+OPENSSL_1_0_ENGINE=libafalg.so
+
+OPENSSL_1_1_ROOT=`readlink -f openssl_dist/root1_1`
+OPENSSL_1_1_CFLAGS="-I${OPENSSL_1_1_ROOT}/usr/include -I${OPENSSL_1_1_ROOT}/usr/include/arm-linux-gnueabi"
+OPENSSL_1_1_LIBS="${OPENSSL_1_1_ROOT}/usr/lib/arm-linux-gnueabi/libcrypto.so.1.1"
+OPENSSL_1_1_ENGINES=${DIST}/usr/lib/arm-linux-gnueabihf/engines-1.1
+OPENSSL_1_1_ENGINE=afalg.so
+
+if [ ! -d ${OPENSSL_1_0_ENGINES} ]; then
+    mkdir -p ${OPENSSL_1_0_ENGINES}
+fi
+
+if [ ! -d ${OPENSSL_1_1_ENGINES} ]; then
+    mkdir -p ${OPENSSL_1_1_ENGINES}
+fi
+
+set -x
+
+${CC} -O2 -pipe -shared -o ${OPENSSL_1_0_ENGINES}/${OPENSSL_1_0_ENGINE} \
+    ciphers.c digests.c e_af_alg.c \
+    -I${DIST}/usr/src/linux/include \
+    ${OPENSSL_1_0_CFLAGS} ${OPENSSL_1_0_LIBS} \
+    -Wl,-no-undefined -Wl,-soname,${OPENSSL_1_0_ENGINE}
+
+cd ${OOT}/af_alg_1_1
+
+${CC} -O2 -pipe -shared -o ${OPENSSL_1_1_ENGINES}/${OPENSSL_1_1_ENGINE} \
+    e_afalg.c e_afalg_err.c \
+    -I${DIST}/usr/src/linux/include \
+    ${OPENSSL_1_1_CFLAGS} ${OPENSSL_1_1_LIBS} \
+    -Wl,-no-undefined -Wl,-soname,${OPENSSL_1_1_ENGINE}
+
+cd -
+
+set +x
+
+echo "[+] Build package"
+cp -r ${OOT}/DEBIAN ${DIST}
+if [ -f ${DIST}.deb ]; then
+    rm -f ${DIST}.deb
+fi
+dpkg-deb --root-owner-group -b ${DIST} ${DIST}.deb
+
+echo "[+] Cleanup"
+rm -rf ${DIST}
+
+echo "[+] DEB: ${DIST}.deb"
